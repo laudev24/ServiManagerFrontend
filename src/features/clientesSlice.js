615ce80ec271f98e7fa6ae4e6d595ecade7f0aa1
nicolopez311 <nlopez.3111@gmail.com>
2025-08-05 14:29:41 -0300

    Correccion ClientesSlice

--- src/features/clientesSlice.js
+++ src/features/clientesSlice.js
@@ -1,23 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    clientes : []
-    
-}
+  clientes: []
+};
+
 export const clientesSlice = createSlice({
-    name: "clientes",
-    initialState,
-    reducers:{
-        guardarClientes: (state, action) => {
-            state.clientes = action.payload;
-        },
-        eliminarCliente: (state, action) => {
-            const idAEliminar = action.payload;
-            state.clientes = state.clientes.filter(m => m.id !== idAEliminar);
-        }
-
+  name: "clientes",
+  initialState,
+  reducers: {
+    guardarClientes: (state, action) => {
+      state.clientes = action.payload;
+    },
+    eliminarCliente: (state, action) => {
+      const id = action.payload;
+      state.clientes = state.clientes.filter(c => c.id !== id);
+    },
+    modificarCliente: (state, action) => {
+      const clienteModificado = action.payload;
+      const index = state.clientes.findIndex(c => c.id === clienteModificado.id);
+      if (index !== -1) {
+        state.clientes[index] = clienteModificado;
+      }
     }
-})
+  }
+});
 
-export const {guardarClientes, eliminarCliente} = clientesSlice.actions;
+export const { guardarClientes, eliminarCliente, modificarCliente } = clientesSlice.actions;
 export default clientesSlice.reducer;
